5be79706dfcadf99a5ecd85ed568b7259962b428
Oleg Sokolov <olegsokolov@Olegs-Air.home>
2016-10-04 18:11:38 -0400

    Assgnment Address Model Remove: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -16,5 +16,16 @@ class AddressBook
         index += 1
       end
       entries.insert(index, Entry.new(name, phone_number, email))
-    end
+  end
+
+  def remove_entry(name, phone_number, email)
+      index = 0
+      entries.each do |entry|
+        if name == entry.name && phone_number == entry.phone_number && email == entry.email
+          break
+        end
+        index += 1
+      end
+      entries.delete_at(index)
+  end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -36,4 +36,33 @@ RSpec.describe AddressBook do
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
     end
   end
+
+  describe "#remove_entry" do
+    it "remove one entry from address book" do
+      book = AddressBook.new
+      book.add_entry('Oleg Sokolov', '012.345.6789', 'ole.sokolov@lovelace.com')
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expect(book.entries.size).to eq(2)
+
+      book.remove_entry('Oleg Sokolov', '012.345.6789', 'ole.sokolov@lovelace.com')
+      expect(book.entries.size).to eq(1)
+    end
+
+    it "remove correct entry from address book" do
+      book = AddressBook.new
+      book.add_entry('Oleg Sokolov', '012.345.6789', 'ole.sokolov@lovelace.com')
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expect(book.entries.size).to eq(2)
+
+      book.remove_entry('Oleg Sokolov', '012.345.6789', 'ole.sokolov@lovelace.com')
+      expect(book.entries.size).to eq(1)
+      
+      new_entry = book.entries[0]
+
+      expect(new_entry.name).to eq('Ada Lovelace')
+      expect(new_entry.phone_number).to eq('010.012.1815')
+      expect(new_entry.email).to eq('augusta.king@lovelace.com')
+
+    end
+  end
 end

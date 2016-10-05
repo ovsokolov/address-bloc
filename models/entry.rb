d087422ecd25664f40ca9206757cc20e7d1514f0
Oleg Sokolov <olegsokolov@Olegs-Air.home>
2016-10-05 17:09:47 -0400

    Checkpoint Address Menu: completed

--- models/entry.rb
+++ models/entry.rb
@@ -5,4 +5,10 @@ class Entry
     @phone_number = phone_number
     @email = email
   end
+
+  def print
+    puts "Name: " + @name
+    puts "Phone: " + @phone_number
+    puts "Email: " + @email
+  end
 end

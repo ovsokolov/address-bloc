00276cbcf38ecf0d6b3b895475e7f3cafddc4118
Oleg Sokolov <olegsokolov@Olegs-Air.home>
2016-10-06 08:27:34 -0400

    Checkpoint Address csv: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry'
+require 'csv'
 
 class AddressBook
   attr_reader :entries
@@ -32,4 +33,16 @@ class AddressBook
   def get_entry_by_number(entry_number)
     return entries[entry_number-1]
   end
+
+  def import_from_csv(file_name)
+      puts Dir.pwd
+      csv_path = './models/' + file_name
+      csv_text = File.read(csv_path)
+      csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
+      csv.each do |row|
+        row_hash = row.to_hash
+        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+      end
+   end
 end

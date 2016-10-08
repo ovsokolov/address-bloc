e9f7dac3eb6e68e2ff70d7418f2258a784493a45
Oleg Sokolov <olegsokolov@Olegs-Air.home>
2016-10-08 10:21:07 -0400

    Checkpoint Addrss Menu Continue: competed

--- models/address_book.rb
+++ models/address_book.rb
@@ -44,6 +44,7 @@ class AddressBook
         row_hash = row.to_hash
         add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
       end
+      return @entries
    end
 
    def binary_search(name)
